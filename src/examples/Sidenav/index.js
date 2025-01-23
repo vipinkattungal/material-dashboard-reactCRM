d3d3cbfbad4c18bac315dac3fb4fed4f7393424a
vipin k <vipinkattungal@gmail.com>
2025-01-23 13:11:22 +0530

    up
    up

--- src/examples/Sidenav/index.js
+++ src/examples/Sidenav/index.js
@@ -189,7 +189,7 @@ function Sidenav({ color, brand, brandName, routes, ...rest }) {
           color={sidenavColor}
           fullWidth
         >
-          upgrade to pro
+          Update Website
         </MDButton>
       </MDBox>
     </SidenavRoot>
